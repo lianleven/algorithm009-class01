ff89a1b973d897efabb63a75b67ed859d4fdf45f
lichangwen <lichangwen@yy.com>
2020-05-24 22:32:20 +0800

    排序数组去重，反转数组

---
+++ Week_01/删除排序数组中的重复项.swift
@@ -0,0 +1,24 @@
+//: [Previous](@previous)
+// https://leetcode-cn.com/problems/remove-duplicates-from-sorted-array/
+// map 方法 一开始理解题意错误，错误解法,
+func removeDuplicates(_ nums: inout [Int]) -> Int {
+    var map: [Int: Int] = [:]
+    for num in nums {
+        map[num] = num
+    }
+    return map.count
+}
+// 正确解法，双指针法
+func removeDuplicates1(_ nums: inout [Int]) -> Int {
+    if nums.count <= 1 {
+        return nums.count
+    }
+    var j = 0
+    for i in 1..<nums.count {
+        if nums[i] != nums[j] {
+            j += 1
+            nums[j] = nums[i]
+        }
+    }
+    return j + 1
+}
